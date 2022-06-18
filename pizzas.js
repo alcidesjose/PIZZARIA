60513a21db2b08b63b713dbbece0d225b690762e
JUNIOR <iqalcides@gmail.com>
2022-06-18 14:01:35 -0300

    Projeto desenvolvido durante o curso

---
+++ pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:28.00, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:32.00, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:37.80, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
+    {id:4, name:'Brasileira', img:'images/pizza4.png', price:39.50, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:42.30, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona e orégano'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:47.50, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
+    {id:7, name:'Chocolate com morango', img:'images/pizza7.png', price:36.70, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Creme de leite, lascas de chocolate e morangos'},
+    {id:8, name:'Banana com canela', img:'images/pizza8.png', price:31.70, sizes:['6 fatias', '8 fatias', '12 fatias'], description:'Mussarela, banana, canela e açúcar'}
+];
